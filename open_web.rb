c03e1f3195f47fc3dbc73087ad2ce44c4ebc0712
魏斗破武 <catback1059@gmail.com>
2013-05-25 23:59:42 +0900

    Update open_web.rb
    わけのわからないcommit

--- open_web.rb
+++ open_web.rb
@@ -12,3 +12,17 @@ Plugin.create(:open_web) do
     end
   end
 end
+
+#webで当該ツイートを見る
+
+Plugin.create(:open_tweet) do
+  command(:open_tweet,
+          name: 'このツイートをwebで見る',
+          condition: Plugin::Command[:HasMessage],
+          visible: true,
+          role: :timeline) do |m|
+    m.messages.map do |msg|
+      Gtk::openurl("https://twitter.com/#{msg.message.user.idname}/status/#{msg.message.message.id}")
+    end
+  end
+end
