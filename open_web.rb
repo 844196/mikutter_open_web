f46f528d025c0028a5b713d19ad2ed5bf8816742
魏斗破武 <catback1059@gmail.com>
2013-05-25 23:36:28 +0900

    Update and rename open_favstar.rb to open_web.rb

--- open_web.rb
+++ open_web.rb
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
-# favstarを開く
+# webでプロフィールを開く
 
 Plugin.create(:open_favstar) do
   command(:open_favstar,
-          name: 'こいつのfavstarを見る',
+          name: 'こいつのプロフィールをwebで見る',
           condition: Plugin::Command[:HasMessage],
           visible: true,
           role: :timeline) do |m|
     m.messages.map do |msg|
-      Gtk::openurl("http://favstar.fm/users/#{msg.message.user.idname}/recent")
+      Gtk::openurl("https://twitter.com/#{msg.message.user.idname}")
     end
   end
 end
