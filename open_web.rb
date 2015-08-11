9087d2235c417b64505d6e77cf6c16393a1b0dea
Masaya Takeda <844196@gmail.com>
2015-08-12 00:21:35 +0900

    プロフィールは標準で満足してるので削除

--- open_web.rb
+++ open_web.rb
@@ -1,18 +1,4 @@
 # -*- coding: utf-8 -*-
-# webでプロフィールを開く
-
-Plugin.create(:open_web) do
-  command(:open_web,
-          name: 'こいつのプロフィールをwebで見る',
-          condition: Plugin::Command[:HasMessage],
-          visible: true,
-          role: :timeline) do |m|
-    m.messages.map do |msg|
-      Gtk::openurl("https://twitter.com/#{msg.message.user.idname}")
-    end
-  end
-end
-
 #webで当該ツイートを見る
 
 Plugin.create(:open_tweet) do
